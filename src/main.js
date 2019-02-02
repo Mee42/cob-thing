533d730fb53b30741ed858afb616f71bbf0aad13
carson <carson42g@gmail.com>
2019-02-02 20:19:38 +0000

    End of 2-2 meeting

--- src/main.js
+++ src/main.js
@@ -4,10 +4,29 @@ const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const ipc = electron.ipcMain;
+
+const wpilib_NT = require('wpilib-nt-client');
+const client = new wpilib_NT.Client();
+
+let connected,
+    ready = false;
 // var ui = require('./ui')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
+
+let clientDataListener = (key, val, valType, mesgType, id, flags) => {
+  if (val === 'true' || val === 'false') {
+      val = val === 'true';
+  }
+  win.webContents.send(mesgType, {
+      key,
+      val,
+      valType,
+      id,
+      flags
+  });
+};
 
 function createWindow () {
   // Create the browser window.
@@ -16,6 +35,47 @@ function createWindow () {
     height: 600,
     show: false
   })
+
+  client.start((con, err) => {
+    // If the Window is ready than send the connection status to it
+    if (ready) {
+      win.webContents.send('connected', con);
+    } else connected = () => win.webContents.send('connected', con);
+});
+// When the script starts running in the window set the ready variable
+ipc.on('ready', (ev, mesg) => {
+    console.log('NetworkTables is ready');
+    ready = win != null;
+
+    // Remove old Listener
+    client.removeListener(clientDataListener);
+
+    // Add new listener with immediate callback
+    client.addListener(clientDataListener, true);
+
+    // Send connection message to the window if if the message is ready
+    if (connected) connected();
+});
+// When the user chooses the address of the bot than try to connect
+ipc.on('connect', (ev, address, port) => {
+  let callback = (connected, err) => {
+      try{
+        win.webContents.send('connected', connected); //throws error ere
+      } catch(e){return;}
+  };
+  if (port) {
+      client.start(callback, address, port);
+  } else {
+      client.start(callback, address);
+      console.log("connecting to " + address)
+  }
+});
+ipc.on('add', (ev, mesg) => {
+  client.Assign(mesg.val, mesg.key, (mesg.flags & 1) === 1);
+});
+ipc.on('update', (ev, mesg) => {
+  client.Update(mesg.id, mesg.val);
+});
 
   // and load the index.html of the app.
   // win.loadFile('index.html')
--- src/renderer.js
+++ src/renderer.js
@@ -32,8 +32,61 @@ let addresses = {
 let ui = {
 	rotation : document.getElementById('rotation'),
 	teleop: document.getElementById('whatsrunning'),
-	timeleft: document.getElementById('timeleft')
+	timeleft: document.getElementById('timeleft'),
+	arm : {
+		canvas : document.getElementById('arm'), //the arm canvass
+	},
+	test: {
+		slider :document.getElementById('angleSlider')
+	},
+	connecter: {
+		address: document.getElementById('connect-address'),
+		connect: document.getElementById('connect')
+	}
 };
+
+
+NetworkTables.addRobotConnectionListener(onRobotConnection, true);
+
+
+function onRobotConnection(connected) {
+	var state = connected ? 'Robot connected!' : 'Robot disconnected.';
+	let address = ui.connecter.address
+	let connect = ui.connecter.connect
+	console.log(state);
+	// ui.robotState.data = state;
+	if (connected) {
+		// On connect hide the connect popups
+		document.body.classList.toggle('login', true);
+	} else {
+		// On disconnect show the connect popup
+		document.body.classList.toggle('login', false);
+		// Add Enter key handler
+		console.log("called onRObotConnection:" + state)
+		address.onkeydown = ev => {
+			if (ev.key === 'Enter') {
+				connect.click();
+			}
+		};
+		// Enable the input and the button
+		address.disabled = false;
+		connect.disabled = false;
+		connect.firstChild.data = 'Connect';
+		// CHANGE THIS VALUE TO YOUR ROBOT'S IP ADDRESS
+		address.value = '10.6.23.2';
+		address.focus();
+		address.setSelectionRange(8, 12);
+		// On click try to connect and disable the input and the button
+		connect.onclick = () => {
+			console.log("connect button clicked")
+			ipc.send('connect', address.value);
+			address.disabled = true;
+			connect.disabled = true;
+			connect.firstChild.data = 'Connecting';
+		};
+	}
+}
+
 
 //should be split into different functions, so that the entire thing doesn't have to render when one thing updates
 //also, values that can be changed and don't touch anything else should (ie, robot vacume on/off) should just be handled in the listener
@@ -42,8 +95,8 @@ let ui = {
 function render(){
 	console.log("Rendering")
 	ui.rotation.textContent = 'Rotation: ' + NetworkTables.getValue(addresses.rotation,'unknown')
-	ui.teleop.textContent = NetworkTables.getValue(addresses.game.teleop,false) ? 'teleop is running' : 
-								NetworkTables.getValue(addresses.game.teleop,false) ? 'Auto is running' : 'Nothing is running'
+	ui.teleop.textContent = NetworkTables.getValue(addresses.robot.isTeleop,false) ? 'teleop is running' : 
+								NetworkTables.getValue(addresses.robot.isSandstorm,false) ? 'Sandstorm is running' : 'Nothing is running'
 	ui.timeleft.textContent = 'Time left: ' + NetworkTables.getValue(addresses.fms.time,'-1')
 
 }
@@ -53,10 +106,42 @@ render()
 // for now, calling it directly after instansiation will work fine
 // without doing this, the screen's objects will not 
 
+function renderArm(){
+	let main = NetworkTables.getValue('' + addresses.arm.mainArm.rotation)
+	main = (main - 90) % 360
+	// let wrist = NetworkTables.getValue(addresses.arm.wrist.rotation)
+	//consts
+	let xMax = 500
+	let yMax = 500
+	let armLength = 200
+	let wristLength = 30
+
+	let ct = ui.arm.canvas.getContext("2d");
+	ct.fillStyle = "silver";
+
+	ct.fillRect(0, 0, ui.arm.canvas.width, ui.arm.canvas.height);
+	ct.beginPath()
+	ct.moveTo(xMax/2,yMax)
+	ct.lineTo(xMax/2,yMax/2);//the middle of the default size
+	let newX = Math.cos(main * (Math.PI / 180))*armLength + xMax/2
+	let newY = Math.sin(main * (Math.PI / 180))*armLength + yMax/2
+	ct.lineTo(newX,newY)
+	ct.stroke()
+}
+renderArm()
+
+NetworkTables.addKeyListener('' + addresses.arm.mainArm.rotation,()=>{
+	renderArm()
+},false);
+NetworkTables.addKeyListener('' + addresses.arm.wrist.wrist,()=>{
+	renderArm()
+},false);
 
 
 
-NetworkTables.addGlobalListener(onValueChanged,true)
+
+
+// NetworkTables.addGlobalListener(onValueChanged,true)//comment out when not used
 
 function onValueChanged(key,value,isNew){
 	// Sometimes, NetworkTables will pass booleans as strings. This corrects for that.
@@ -65,7 +150,6 @@ function onValueChanged(key,value,isNew){
 	} else if (value === 'false') {
 		value = false;
 	}
-	render();//render every time something new happens. Should be removed when render() is refactored into listeners
 }
 
 
