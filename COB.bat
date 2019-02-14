67837774e8c1ddda90952f56c5761849691efc2b
Carson Graham <carson42g@gmail.com>
2019-02-14 20:45:18 +0000

    added pull to bat

--- COB.bat
+++ COB.bat
@@ -2,8 +2,10 @@ cd "C:\Program Files (x86)\FRC Driver Station\"
 
 start DriverStation.exe
 
-cd "C:\Users\frc623\COB-2019
+cd "C:\Users\frc623\COB-2019"
 
 git checkout carson
+
+git pull
 
 npm start 
